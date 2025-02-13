84d9ace9902f60a0c90678cb0a78c3e477c32bc2
IvanGadus <106027737+IvanGadus@users.noreply.github.com>
2025-02-13 09:43:19 +0100

    Update form button onClick

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function FormButton({ id }: { id: number }) {
 	return (
 		<button
-			onClick={() => deleteExpense(id)}
+			onClick={async () => await deleteExpense(id)}
 			className="text-[10px] h-[20px] w-[20px] bg-red-500 text-white rounded hover:bg-red-600"
 		>
 			X
